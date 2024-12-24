2e94abb709f91a87323eee6030363884c035fedc
devanps212 <devanps212@gmail.com>
2024-12-24 11:47:28 +0530

    added constants

---
+++ e2e/constant/selector/formHeader.ts
@@ -0,0 +1,5 @@
+export const FORM_HEADER_SELECTORS = {
+    header:'neeto-molecules-header',
+    passwordHeading:'[data-cy="password-protected-heading"]',
+    welcomeScreen:'[data-cy="welcome-screen-group"]'
+}
---
+++ e2e/constant/selector/index.ts
@@ -0,0 +1,7 @@
+import { FORM_HEADER_SELECTORS } from "./formHeader";
+import { FORM_LABELS } from "../text";
+
+export {
+    FORM_HEADER_SELECTORS,
+    FORM_LABELS
+}
