aec6bc913403f6806de4579bde923c9f8969e99a
devanps212 <devanps212@gmail.com>
2024-12-25 17:28:21 +0530

    imported texts to index file

--- e2e/constant/text/index.ts
+++ e2e/constant/text/index.ts
@@ -1,9 +1,13 @@
-import { FORM_LABELS } from "./formLabels";
+import { FORM_LABELS, FORM_LABEL1, FORM_LABEL2 } from "./formLabels";
+import { ROW_COL } from "./tableData";
 // import { SUBMISSION_USER_DETAILS } from "./userSubmission";
 // import { SUCCESS_MESSAGE } from "./successMessage";
 
 export {
     FORM_LABELS,
+    ROW_COL,
+    FORM_LABEL1,
+    FORM_LABEL2
     // SUBMISSION_USER_DETAILS,
     // SUCCESS_MESSAGE
 }
